4d08f5b7f3f20b86bdb11af37cf7f38de9da7ed0
Bridiro <ale.bridi15@gmail.com>
2026-03-14 23:43:17 +0100

    feat: fff for inputs as well #7

--- test/cm4/test_inputs/test_inputs.c
+++ test/cm4/test_inputs/test_inputs.c
@@ -1,27 +1,23 @@
 #include "inputs-api.h"
 #include "inputs-shared.h"
+#include "fff.h"
 #include "unity.h"
 
-int notify_callback_call_count = 0;
-int action_callback_call_count = 0;
+DEFINE_FFF_GLOBALS;
 
-void critical_section_callback(void) {
-}
+typedef void (*cs_callback)(void);
 
-bool notify_callback(struct InputsSharedEvent ev, void (*critical_section_callback)(void)) {
-    notify_callback_call_count++;
-
-    return true;
-}
-
-enum InputsReturnCode action_callback(struct InputsSharedEvent ev) {
-    action_callback_call_count++;
-    return INPUTS_RC_OK;
-}
+FAKE_VOID_FUNC(critical_section_callback);
+FAKE_VALUE_FUNC(bool, notify_callback, struct InputsSharedEvent, cs_callback);
+FAKE_VALUE_FUNC(enum InputsReturnCode, action_callback, struct InputsSharedEvent);
 
 struct InputsHandler handler;
 
 void setUp(void) {
+    RESET_FAKE(critical_section_callback);
+    RESET_FAKE(notify_callback);
+    RESET_FAKE(action_callback);
+    FFF_RESET_HISTORY();
     inputs_api_init(&handler, critical_section_callback, notify_callback, action_callback);
 }
 
@@ -49,11 +45,13 @@ void test_inputs_init_fail(void) {
  * @{
  */
 
-void test_inputs_update_button_simple(void) {
+void test_inputs_update_button_success(void) {
+    notify_callback_fake.return_val = true;
+    action_callback_fake.return_val = INPUTS_RC_OK;
     enum InputsReturnCode ret = inputs_api_update_button(&handler, INPUTS_SHARED_BUTTON_ID_BOTTOM_LEFT, true, 100);
     TEST_ASSERT_EQUAL_MESSAGE(INPUTS_RC_OK, ret, "Failed to update button state");
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, notify_callback_call_count, "Notify callback was not called");
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, action_callback_call_count, "Action callback was not called");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, notify_callback_fake.call_count, "Notify callback was not called");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, action_callback_fake.call_count, "Action callback was not called");
 }
 
 /*! @} */
@@ -64,7 +62,7 @@ int main() {
     RUN_TEST(test_inputs_init_success);
     RUN_TEST(test_inputs_init_fail);
 
-    RUN_TEST(test_inputs_update_button_simple);
+    RUN_TEST(test_inputs_update_button_success);
 
     return UNITY_END();
 }
