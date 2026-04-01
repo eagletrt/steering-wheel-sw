be4faa537c7bb873481a5fdc85855d55ded8eef8
Bridiro <ale.bridi15@gmail.com>
2026-04-01 20:43:47 +0200

    feat: post for CM7 #8

---
+++ CM7/Core/Inc/steering/post-api.h
@@ -0,0 +1,25 @@
+/*!
+ * \file post-api.h
+ * \date 2026-04-01
+ * \authors Alessandro Bridi [ale.bridi15@gmail.com]
+ *
+ * \brief This file defines Power-On Self-Test (POST) functions for system diagnostics.
+ */
+
+#ifndef POST_API_H
+#define POST_API_H
+
+#include "post.h"
+
+/*!
+ * \brief Performs the Power-On Self-Test (POST) using the provided initialization data.
+ *
+ * This function initializes all the modules required by the steering wheel firmware.
+ *
+ * \retval POST_RC_OK if POST completed successfully
+ * \retval POST_RC_ERROR_MINOR if POST encountered a minor error
+ * \retval POST_RC_ERROR_SEVERE if POST encountered a severe error
+ */
+enum PostReturnCode post_api_do_init(struct PostInitData *post_init_data);
+
+#endif // POST_API_H
