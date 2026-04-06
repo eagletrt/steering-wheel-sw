4628124d016637740fc16b6a327e22144ca0c6dd
Bridiro <ale.bridi15@gmail.com>
2026-04-06 17:23:48 +0200

    feat: add ingroup to post

--- CM7/Core/Inc/steering/post.h
+++ CM7/Core/Inc/steering/post.h
@@ -2,6 +2,7 @@
  * \file post.h
  * \date 2026-04-01
  * \authors Alessandro Bridi [ale.bridi15@gmail.com]
+ * \ingroup CM7_Core
  *
  * \brief This file defines Power-On Self-Test (POST) structures for system diagnostics.
  */
